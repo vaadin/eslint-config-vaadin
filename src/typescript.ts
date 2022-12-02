bf58e6a79ad71e3f5d9cd894acfd232770082352
Sergey Vinogradov <mr.vursen@gmail.com>
2022-12-02 16:16:20 +0200

    refactor: rename index.js to javascript.js (#22)

--- src/typescript.ts
+++ src/typescript.ts
@@ -1,7 +1,7 @@
 const resolve = require('./utils/resolve.js');
 
 export = {
-  extends: ['./index', './rules/typescript/original', './rules/typescript/extensions'].map(resolve),
+  extends: ['./javascript', './rules/typescript/original', './rules/typescript/extensions'].map(resolve),
   settings: {
     'import/resolver': {
       node: {
