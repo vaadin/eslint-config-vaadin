a6622deaa18edacd52d3b91fae27e37f621fa70a
Sergey Vinogradov <mr.vursen@gmail.com>
2023-01-31 14:52:39 +0200

    fix: disable typescript-eslint/no-unused-expressions in tests (#37)

--- src/rules/testing.ts
+++ src/rules/testing.ts
@@ -2,6 +2,7 @@ export = {
   plugins: ['chai-friendly'],
   rules: {
     'no-unused-expressions': 'off',
+    '@typescript-eslint/no-unused-expressions': 'off',
     'chai-friendly/no-unused-expressions': 'error',
     'max-classes-per-file': 'off',
   }
