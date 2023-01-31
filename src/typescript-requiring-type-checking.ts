027ec0ba4c7886f149387dc17bf78d3272ba2274
Sergey Vinogradov <mr.vursen@gmail.com>
2023-01-31 17:05:46 +0200

    remove file extensions added by a mistake

--- src/typescript-requiring-type-checking.ts
+++ src/typescript-requiring-type-checking.ts
@@ -3,8 +3,8 @@ const resolve = require('./utils/resolve.js');
 export = {
   extends: [
     './javascript',
-    './rules/typescript-requiring-type-checking/original.ts',
-    './rules/typescript-requiring-type-checking/extensions.ts',
+    './rules/typescript-requiring-type-checking/original',
+    './rules/typescript-requiring-type-checking/extensions',
   ].map(resolve),
   settings: {
     'import/resolver': {
