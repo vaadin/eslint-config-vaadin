130fde2b887d7f757781fc28e997ee0535bc5e3e
Vlad Rindevich <vladrin@vaadin.com>
2023-08-02 18:49:21 +0300

    refactor: add disabled rules and reason to disable them

--- src/sort.ts
+++ src/sort.ts
@@ -1,10 +1,27 @@
 export = {
   plugin: ['perfectionist'],
   rules: {
+    // sorting array will definitely break the code somewhere.
+    'perfectionist/sort-array-includes': 'off',
+    // handled by "@typescript-eslint/member-ordering"
+    'perfectionist/sort-classes': 'off',
     'perfectionist/sort-enums': ['error', { type: 'natural', order: 'asc', 'ignore-case': true }],
     'perfectionist/sort-exports': ['error', { type: 'natural', order: 'asc', 'ignore-case': true }],
+    // handled by "eslint-plugin-import" rules
+    'perfectionist/sort-imports': 'off',
+    // does not respect "@typescript-eslint/member-ordering" rules
+    'perfectionist/sort-interfaces': 'off',
     'perfectionist/sort-jsx-props': ['error', { type: 'natural', order: 'asc', 'ignore-case': true }],
+    // disabled by default; enable if necessary
+    'perfectionist/sort-map-elements': 'off',
     'perfectionist/sort-named-exports': ['error', { type: 'natural', order: 'asc', 'ignore-case': true }],
+    // handled by "eslint-plugin-import" rules
+    'perfectionist/sort-named-imports': 'off',
+    // does not respect "@typescript-eslint/member-ordering" rules
+    'perfectionist/sort-object-types': 'off',
     'perfectionist/sort-objects': ['error', { type: 'natural', order: 'asc', 'ignore-case': true }],
+    // handled by "@typescript-eslint/sort-type-constituents" and "@typescript-eslint/sort-type-union-intersection-members"
+    // rules
+    'perfectionist/sort-union-types': 'off'
   },
 } as const;
