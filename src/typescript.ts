691c2e586dddbf955fb2399be7ee24d03a6b3d14
Sergey Vinogradov <mr.vursen@gmail.com>
2023-09-13 16:40:01 +0300

    fix: remove parserOptions.project from vaadin/typescript

--- src/typescript.ts
+++ src/typescript.ts
@@ -1,8 +1,5 @@
 export = {
   extends: ['./javascript', './rules/typescript/original', './rules/typescript/extensions'],
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: `${process.cwd()}/tsconfig.json`,
-  },
   plugins: ['@typescript-eslint'],
 } as const;
