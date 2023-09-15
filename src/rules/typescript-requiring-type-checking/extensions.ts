22d4b5d95cf1c3719325775ae853fc1b0e61d504
Vlad Rindevich <vladrin@vaadin.com>
2023-09-15 13:28:42 +0300

    fix: remove deprecated rule

--- src/rules/typescript-requiring-type-checking/extensions.ts
+++ src/rules/typescript-requiring-type-checking/extensions.ts
@@ -5,16 +5,12 @@ export = {
     '@typescript-eslint/no-implied-eval': suggestions['no-implied-eval'],
     '@typescript-eslint/no-throw-literal': suggestions['no-throw-literal'],
     '@typescript-eslint/require-await': suggestions['require-await'],
-    '@typescript-eslint/return-await': suggestions['no-return-await'],
     // Enforce dot notation whenever possible
     // https://typescript-eslint.io/rules/dot-notation
     'dot-notation': 'off',
     // Disallow the use of eval()-like methods
     // https://typescript-eslint.io/rules/no-implied-eval
     'no-implied-eval': 'off',
-    // Enforces consistent returning of awaited values
-    // https://typescript-eslint.io/rules/return-await
-    'no-return-await': 'off',
     // Disallow throwing literals as exceptions
     // https://typescript-eslint.io/rules/no-throw-literal
     'no-throw-literal': 'off',
