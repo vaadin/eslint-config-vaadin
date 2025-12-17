d02fe63f3b9fc386872513c4f0276b99ac101fcb
Vlad Rindevich <github.aqwb5@mail.ausginer.com>
2025-12-17 16:31:16 +0000

    chore: add devcontainer

--- tools/missing-rule-checker/index.ts
+++ tools/missing-rule-checker/index.ts
@@ -1,14 +1,26 @@
-import puppeteer from 'puppeteer-core';
+import dns from 'node:dns/promises';
+import puppeteer, { type Browser } from 'puppeteer-core';
 import checkEslint from './eslint.js';
 import checkImports from './imports.js';
 import checkTypeScript from './typescript.js';
 
-if (!process.env['CHROME_BIN']) {
-  throw new Error('"CHROME_BIN" environment variable is not set');
-}
-
 try {
-  const browser = await puppeteer.launch({ headless: true, executablePath: process.env['CHROME_BIN'] });
+  let browser: Browser;
+
+  if (process.env['CHROME_BIN']) {
+    browser = await puppeteer.launch({
+      headless: true,
+      executablePath: process.env['CHROME_BIN'],
+    });
+  } else if (process.env['BROWSER_HOST'] && process.env['BROWSER_PORT']) {
+    const { address } = await dns.lookup(process.env['BROWSER_HOST']);
+
+    browser = await puppeteer.connect({
+      browserURL: `http://${address}:${process.env['BROWSER_PORT']}`,
+    });
+  } else {
+    throw new Error('Neither "CHROME_BIN" nor "BROWSER_URL" environment variable is set');
+  }
 
   const results = await Promise.all([checkEslint(browser), checkTypeScript(browser), checkImports(browser)]);
 
